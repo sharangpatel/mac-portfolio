f063fa398bde038ca03b5cbf5dacf56c52880d6c
Sharang Patel <sharangpatel@Sharangs-MacBook-Air.local>
2025-12-04 18:11:15 +0530

    implement dock-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '#components/Navbar'
 import Welcome from '#components/Welcome'
+import Dock from '#components/Dock'
 
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Welcome/>
+      <Dock/>
     </main>
   )
 }
